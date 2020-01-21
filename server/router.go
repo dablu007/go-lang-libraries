ddd594e0bee718736adf4ddb2c7326bbdc292ae3
shalini <shalini@zestmoney.in>
2020-01-21 12:21:24 +0530

    NUC-3: Corrected config and added authentication for flow api

--- server/router.go
+++ server/router.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"flow/auth"
 	"flow/controller"
 
 	"github.com/gin-gonic/gin"
@@ -10,11 +11,10 @@ func NewRouter() *gin.Engine {
 	router := gin.New()
 	router.Use(gin.Logger())
 	router.Use(gin.Recovery())
-	// logger.InitLogger()
 	health := new(controller.HealthController)
 
 	router.GET("flow/health", health.Status)
-	// router.Use(auth.AuthMiddleware())
+	router.Use(auth.AuthMiddleware())
 
 	v1 := router.Group("flow/api/v1")
 	{
