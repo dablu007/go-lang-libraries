5a34ea3b40c587ae1686a7e3e1d4f6ee0d118b0a
shalini <shalini@zestmoney.in>
2020-01-21 12:46:33 +0530

    NUC-3: reduced db calls.

--- service/flow_service.go
+++ service/flow_service.go
@@ -8,6 +8,7 @@ import (
 	"flow/logger"
 	"flow/model"
 	"flow/model/response_dto"
+	"fmt"
 )
 
 type FlowService struct {
@@ -71,7 +72,94 @@ func FetchAllFlowsFromDB(flowContext model.FlowContext) []model.Flow {
 
 func GetParsedFlowsResponse(flows []model.Flow) (response_dto.FlowResponsesDto, error) {
 	dbConnection := db.GetDB()
+	completeModuleVersionNumberList := make(map[int]bool)
+	var moduleVersions []model.ModuleVersion
+	moduleVersionsMap := make(map[int]model.ModuleVersion)
+
+	completeSectionVersionNumberList := make(map[int]bool)
+	var sectionVersions []model.SectionVersion
+	sectionVersionsMap := make(map[int]model.SectionVersion)
+
+	completeFieldVersionNumberList := make(map[int]bool)
+	var fieldVersions []model.FieldVersion
+	fieldVersionsMap := make(map[int]model.FieldVersion)
+
 	var response response_dto.FlowResponsesDto
+	for _, flow := range flows {
+		var versionNumbers []int
+		json.Unmarshal([]byte(flow.ModuleVersions), &versionNumbers)
+		for _, num := range versionNumbers {
+			if completeModuleVersionNumberList[num] == false {
+				completeModuleVersionNumberList[num] = true
+			}
+		}
+	}
+
+	keys := []int{}
+	for k, _ := range completeModuleVersionNumberList {
+		keys = append(keys, k)
+	}
+
+	fmt.Println(keys)
+
+	dbConnection.Debug().Joins("JOIN modules ON modules.id = module_versions.module_id and modules.status = ? and modules.deleted_on is NULL", enum.Active).Where("module_versions.id in (?) and module_versions.deleted_on is NULL", keys).Find(&moduleVersions)
+
+	for _, mv := range moduleVersions {
+		if (moduleVersionsMap[mv.Id] == model.ModuleVersion{}) {
+			moduleVersionsMap[mv.Id] = mv
+		}
+	}
+
+	for _, mv := range moduleVersions {
+		var sectionNumbers []int
+		json.Unmarshal([]byte(mv.SectionVersions), &sectionNumbers)
+		for _, num := range sectionNumbers {
+			if completeSectionVersionNumberList[num] == false {
+				completeSectionVersionNumberList[num] = true
+			}
+		}
+	}
+
+	keys1 := []int{}
+	for k, _ := range completeSectionVersionNumberList {
+		keys1 = append(keys1, k)
+	}
+
+	fmt.Println(keys1)
+
+	dbConnection.Debug().Joins("JOIN sections ON sections.id = section_versions.section_id and sections.status = ? and sections.deleted_on is NULL", enum.Active).Where("section_versions.id in (?) and section_versions.deleted_on is NULL", keys1).Find(&sectionVersions)
+
+	for _, sv := range sectionVersions {
+		if (sectionVersionsMap[sv.Id] == model.SectionVersion{}) {
+			sectionVersionsMap[sv.Id] = sv
+		}
+	}
+
+	for _, sv := range sectionVersions {
+		var fieldNumbers []int
+		json.Unmarshal([]byte(sv.FieldVersions), &fieldNumbers)
+		for _, num := range fieldNumbers {
+			if completeFieldVersionNumberList[num] == false {
+				completeFieldVersionNumberList[num] = true
+			}
+		}
+	}
+
+	keys2 := []int{}
+	for k, _ := range completeFieldVersionNumberList {
+		keys2 = append(keys2, k)
+	}
+
+	fmt.Println(keys2)
+
+	dbConnection.Debug().Joins("JOIN fields ON fields.id = field_versions.field_id and fields.status = ? and fields.deleted_on is NULL", enum.Active).Where("field_versions.id in (?) and field_versions.deleted_on is NULL", keys2).Find(&fieldVersions)
+
+	for _, fv := range fieldVersions {
+		if (fieldVersionsMap[fv.Id] == model.FieldVersion{}) {
+			fieldVersionsMap[fv.Id] = fv
+		}
+	}
+
 	for _, flow := range flows {
 		flowResponseDto := response_dto.FlowResponseDto{
 			Name:       flow.Name,
@@ -80,50 +168,44 @@ func GetParsedFlowsResponse(flows []model.Flow) (response_dto.FlowResponsesDto, 
 			Type:       flow.Type}
 		var moduleVersionNumberList []int
 		json.Unmarshal([]byte(flow.ModuleVersions), &moduleVersionNumberList)
-		for i := 0; i < len(moduleVersionNumberList); i++ {
-			var moduleVersion model.ModuleVersion
-			dbConnection.Debug().Joins("JOIN modules ON modules.id = module_versions.module_id and modules.status = ? and modules.deleted_on is NULL", enum.Active).Where("module_versions.id = ? and module_versions.deleted_on is NULL", moduleVersionNumberList[i]).Find(&moduleVersion)
-			if (model.ModuleVersion{}) == moduleVersion {
-				continue
-			}
-			moduleVersionResponseDto := response_dto.ModuleVersionResponseDto{
-				Name:       moduleVersion.Name,
-				ExternalId: moduleVersion.ExternalId,
-				Version:    moduleVersion.Version}
-			json.Unmarshal([]byte(moduleVersion.Properties), &moduleVersionResponseDto.Properties)
-			var sectionVersionNumberList []int
-			json.Unmarshal([]byte(moduleVersion.SectionVersions), &sectionVersionNumberList)
-			for i := 0; i < len(sectionVersionNumberList); i++ {
-				var sectionVersion model.SectionVersion
-				dbConnection.Debug().Joins("JOIN sections ON sections.id = section_versions.section_id and sections.status = ? and sections.deleted_on is NULL", enum.Active).Where("section_versions.id = ? and section_versions.deleted_on is NULL", sectionVersionNumberList[i]).Find(&sectionVersion)
-				if (model.SectionVersion{}) == sectionVersion {
-					continue
-				}
-				sectionVersionResponseDto := response_dto.SectionVersionsResponseDto{
-					Name:       sectionVersion.Name,
-					ExternalId: sectionVersion.ExternalId,
-					Version:    sectionVersion.Version,
-					IsVisible:  sectionVersion.IsVisible}
-				json.Unmarshal([]byte(sectionVersion.Properties), &sectionVersionResponseDto.Properties)
-				var fieldVersionNumberList []int
-				json.Unmarshal([]byte(sectionVersion.FieldVersions), &fieldVersionNumberList)
-				for i := 0; i < len(fieldVersionNumberList); i++ {
-					var fieldVersion model.FieldVersion
-					dbConnection.Debug().Joins("JOIN fields ON fields.id = field_versions.field_id and fields.status = ? and fields.deleted_on is NULL", enum.Active).Where("field_versions.id = ? and field_versions.deleted_on is NULL", fieldVersionNumberList[i]).Find(&fieldVersion)
-					if (model.FieldVersion{}) == fieldVersion {
-						continue
+		for _, mvn := range moduleVersionNumberList {
+			if completeModuleVersionNumberList[mvn] == true {
+				moduleVersion := moduleVersionsMap[mvn]
+				moduleVersionResponseDto := response_dto.ModuleVersionResponseDto{
+					Name:       moduleVersion.Name,
+					ExternalId: moduleVersion.ExternalId,
+					Version:    moduleVersion.Version}
+				json.Unmarshal([]byte(moduleVersion.Properties), &moduleVersionResponseDto.Properties)
+				var sectionVersionNumberList []int
+				json.Unmarshal([]byte(moduleVersion.SectionVersions), &sectionVersionNumberList)
+				for _, svn := range sectionVersionNumberList {
+					if completeSectionVersionNumberList[svn] == true {
+						sectionVersion := sectionVersionsMap[svn]
+						sectionVersionResponseDto := response_dto.SectionVersionsResponseDto{
+							Name:       sectionVersion.Name,
+							ExternalId: sectionVersion.ExternalId,
+							Version:    sectionVersion.Version,
+							IsVisible:  sectionVersion.IsVisible}
+						json.Unmarshal([]byte(sectionVersion.Properties), &sectionVersionResponseDto.Properties)
+						var fieldVersionNumberList []int
+						json.Unmarshal([]byte(sectionVersion.FieldVersions), &fieldVersionNumberList)
+						for _, fvn := range fieldVersionNumberList {
+							if completeFieldVersionNumberList[fvn] == true {
+								fieldVersion := fieldVersionsMap[fvn]
+								fieldVersionResponseDto := response_dto.FieldVersionsResponseDto{
+									Name:       fieldVersion.Name,
+									ExternalId: fieldVersion.ExternalId,
+									IsVisible:  fieldVersion.IsVisible,
+									Version:    fieldVersion.Version}
+								json.Unmarshal([]byte(fieldVersion.Properties), &fieldVersionResponseDto.Properties)
+								sectionVersionResponseDto.Fields = append(sectionVersionResponseDto.Fields, fieldVersionResponseDto)
+							}
+						}
+						moduleVersionResponseDto.Sections = append(moduleVersionResponseDto.Sections, sectionVersionResponseDto)
 					}
-					fieldVersionResponseDto := response_dto.FieldVersionsResponseDto{
-						Name:       fieldVersion.Name,
-						ExternalId: fieldVersion.ExternalId,
-						IsVisible:  fieldVersion.IsVisible,
-						Version:    fieldVersion.Version}
-					json.Unmarshal([]byte(fieldVersion.Properties), &fieldVersionResponseDto.Properties)
-					sectionVersionResponseDto.Fields = append(sectionVersionResponseDto.Fields, fieldVersionResponseDto)
 				}
-				moduleVersionResponseDto.Sections = append(moduleVersionResponseDto.Sections, sectionVersionResponseDto)
+				flowResponseDto.Modules = append(flowResponseDto.Modules, moduleVersionResponseDto)
 			}
-			flowResponseDto.Modules = append(flowResponseDto.Modules, moduleVersionResponseDto)
 		}
 		response.FlowResponses = append(response.FlowResponses, flowResponseDto)
 	}
