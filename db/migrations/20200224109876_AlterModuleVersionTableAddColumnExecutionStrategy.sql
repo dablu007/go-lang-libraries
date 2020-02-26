1d9f918ce68332bb99730f6527a78b8129773344
shalini <shalini@zestmoney.in>
2020-02-26 11:01:57 +0530

    NUC-157: added goose down script.

--- db/migrations/20200224109876_AlterModuleVersionTableAddColumnExecutionStrategy.sql
+++ db/migrations/20200224109876_AlterModuleVersionTableAddColumnExecutionStrategy.sql
@@ -1,4 +1,11 @@
 -- +goose Up
 -- SQL in this section is executed when the migration is applied.
 
-alter table module_version add column execution_strategy smallint not null default 1;
+ALTER TABLE module_version ADD COLUMN execution_strategy SMALLINT NOT NULL DEFAULT 1;
+
+
+
+-- +goose Down
+-- SQL section 'Down' is executed when this migration is rolled back.
+
+ALTER TABLE module_version DROP COLUMN execution_strategy;
